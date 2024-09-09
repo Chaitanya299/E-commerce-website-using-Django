c2591c2fce86f7d55f211200529933c05ea71ece
Chaitanya299 <92148748+Chaitanya299@users.noreply.github.com>
2024-09-09 19:53:58 +0530

    first push

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Value, F, Func, Q
+from django.db.models.aggregates import Count,Max,Min,Avg,Sum
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product, orderItem
 
 # Create your views(action) here.
 # request handler 
 # action or views
 
-def calculate():
-    x=1
-    y=2
-    return x
-def say_hello(request):
-    x=calculate()
-    return render(request,'hello.html')
 
+def say_hello(request):
+    
+    queryset=Product.objects.order_by('-title')
+   
+    return render(request,'hello.html',{'name':'chaitu','products':list(queryset)})
+
+  
